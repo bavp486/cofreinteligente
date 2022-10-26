b185296d5a9a55f7dc8edddb1904f16e30a8a417
Bruno Augusto <bavp486@gmail.com>
2022-10-25 21:24:08 -0300

    Initial commit

---
+++ src/main/java/br/com/bra/cofreinteligente/CofreinteligenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.bra.cofreinteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofreinteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CofreinteligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bra/cofreinteligente/CofreinteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bra.cofreinteligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofreinteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
