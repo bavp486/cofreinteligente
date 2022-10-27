b444aee97520fcdb8957907aec79052fd5759357
Bruno Augusto <bavp486@gmail.com>
2022-10-27 11:10:35 -0300

    Criação de Repository e alguns acertos em variaveis

--- src/main/java/br/com/bra/cofreinteligente/controller/ClienteController.java
+++ src/main/java/br/com/bra/cofreinteligente/controller/ClienteController.java
@@ -1,5 +1,10 @@
 package br.com.bra.cofreinteligente.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Cliente")
 public class ClienteController {
 
 }
